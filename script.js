469c0b5884110276cfe870d979ff345d353e04b4
Julicristina <132705845+Julicristina@users.noreply.github.com>
2024-07-03 16:39:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+
 
 const perguntas = [
     {
@@ -63,6 +59,20 @@ const perguntas = [
         ]
     },
     {
+        enunciado: "Sobre o Enigma",
+        alternativas: [
+            {
+                texto: "Se escolheu Usar seu conhecimento sobre ingredientes raros: ",
+                afirmacao: "Você decifra o enigma e encontra uma pista crucial sobre a localização do ingrediente lendário."
+            },
+            {
+                texto: "Se escolheu Pedir ajuda ao estranho que mencionou o ingrediente lendário: ",
+                afirmacao: "O estranho revela informações valiosas que o ajudam a desvendar o mistério do ingrediente lendário, mas não ajuda em muita coisa"
+            }
+        ]
+    },
+
+     {
         enunciado: "Sobre o Guardião:",
         alternativas: [
             {
@@ -75,34 +85,8 @@ const perguntas = [
             }
         ]
     },
-     {
-        enunciado: "Se você escolheu o caminho sombrio e misterioso. ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
-    },
+   
 ];
-
 
 let atual = 0;
 let perguntaAtual;
